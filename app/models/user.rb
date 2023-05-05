e2bbd3206756b494adaa0b62f1b0d13fe06c7db0
HaradaSeira <starcomes19870920@gmail.com>
2023-05-05 04:28:44 +0000

    Rails 04 User新規登録に潜む問題を解決していく課題提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+ validates :name, presence: true, length: { maximum: 15 }
+ validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, }
+ validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,32}\z/ , }
+ 
+ has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_28_015734) do
+ActiveRecord::Schema.define(version: 2023_05_04_092519) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
