d257d754a6c075dae6854bcc359244e43b3d9806
HaradaSeira <starcomes19870920@gmail.com>
2023-05-17 12:43:35 +0000

    Rails 07 「いいね」機能の作成の課題

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,19 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.all.includes(:favorite_users)
+    @topics = Topic.all.includes(:favorite_users, :comment_users)
   end
   
   def new
     @topic = Topic.new
   end
-
+  
   def create
     @topic = current_user.topics.new(topic_params)
     
     if @topic.save
       redirect_to topics_path, success: '投稿に成功しました'
     else
-      flash.now[:danger] = '投稿に失敗しました'
+      flash.now[:danger] = "投稿に失敗しました"
       render :new
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,27 @@
 Rails.application.routes.draw do
-  get   'sessions/new'
-  get   'users/new'
-
-  root  'pages#index'
-  get   'pages/help'
-  get   'pages/link'
-  
-  get     '/login',   to: 'sessions#new'
-  post    '/login',   to: 'sessions#create'
-  delete  '/logout',  to: 'sessions#destroy'
-
-  resources :users
-  resources :topics
-  
-  get     'favorites/index'
-  post    '/favorites', to: 'favorites#create'
-  delete  '/favorites', to: 'favorites#destroy'
+  get 'topics/new'
+  get 'sessions/new'
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/help'
+  get 'pages/link'
+  
+  resources :users
+  resources :topics 
+  
+  get 'comments/new'
+  post '/comments', to: 'comments#create'
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+  
+ resource :comments
+  
+  get     '/login',  to: 'sessions#new'
+  post    '/login',  to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+  
+ 
 end
